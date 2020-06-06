fb88ab3414953aa38ddd1e7919b0b334e3f20ef8
Fatoumata A Diaby <fatimad5293@gmail.com>
2020-06-06 16:15:05 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -99,12 +99,9 @@ end
 adding_matz
 
 
-
-
-
 def changing_alan
-	programmer_hash = 
-		{
+	programmer_hash =
+ 		{
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
@@ -117,12 +114,18 @@ def changing_alan
           :known_for => "Unix",
           :languages => ["C"]
         }
-    }
-    #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
-    alans_new_info = "GUI"
+     }
+     #change what Alan Kay is :known_for to the value of the alans_new_info variable.
+     alans_new_info = "GUI"
      
-     
+  programmer_hash[:alan_kay][:known_for] = "GUI"
+  programmer_hash
+
 end
+
+changing_alan
+
+
 
 def adding_to_dennis
 	programmer_hash = 
